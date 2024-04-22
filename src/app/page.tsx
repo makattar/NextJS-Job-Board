d31b5dd047cb5b1f633f76c07e083991709521a3
Mahamad Abdulkadar <mahamad.abdulkadar@baxture.com>
2024-04-22 14:25:01 +0530

    Job List Component Created with Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,20 @@ export default async function Home() {
   const { getAllByApproved } = JobRepository();
   const jobs = await getAllByApproved(true);
   return (
-    <main>
-      {jobs.map((job) => {
-        return <JobListItem job={job} />;
-      })}
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl  ">
+          Developer jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => {
+            return <JobListItem job={job} />;
+          })}
+        </div>
+      </section>
     </main>
   );
 }
